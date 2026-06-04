a75319734046ba0dcc061f84ea8c24f31fd142c8
Nazri Sumarato <115539507+loadedneon@users.noreply.github.com>
2026-06-04 20:12:41 +0800

    Update tester information in PageNotFoundException

--- PageNotFoundException.java
+++ PageNotFoundException.java
@@ -1,6 +1,6 @@
 /**
  * Creator: Siti Nur Amira binti Zulkiply
- * Tester: Rosaliny Lisa anak Roza (106166), Mohamad Nazri Bin Sumarato (84546), Mohamad Nazri Bin Sumarato(845553)
+ * Tester: Rosaliny Lisa anak Roza (106166), Mohamad Nazri Bin Sumarato (84546), Mohamad Syahmi bin Soria (84553)
  * Description: Custom exception to handle errors if a page doesn't exist.
  */
 public class PageNotFoundException extends Exception {
